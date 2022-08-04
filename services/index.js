13437c2aeb8581b1c049fd2495159e271a29058f
Eduardo <eduhmq@gmail.com>
2022-08-04 20:57:24 -0300

    Finished map feature, finished banner data showing

--- services/index.js
+++ services/index.js
@@ -8,10 +8,61 @@ async function handleUserIp() {
     const ip = await this.getIp();
     console.log(ip)
     document.getElementById("ip_final").innerText = ip;
-    
+    doSearch();
     return ip;
 }
 
+async function locateIp() {
+    let ipUrl = 'https://geo.ipify.org/api/v2/country,city?apiKey=at_5FgIKynfFiC4oIrWhRgEzEbHfaTvu&ipAddress='.concat(await handleUserIp());
+    let response = await fetch(ipUrl)
+    response = await response.json()
+    return response;
+}
+
+async function doSearch() {
+    let ipToSearch = document.getElementById('ip_input').value;
+    console.log(ipToSearch);
+    let ipUrl = 'https://geo.ipify.org/api/v2/country,city?apiKey=at_5FgIKynfFiC4oIrWhRgEzEbHfaTvu&ipAddress='.concat(ipToSearch);
+    console.log(ipUrl);
+    response = await fetch(ipUrl);
+    response = await response.json();
+    document.getElementById('ip_final').innerText = response.ip;
+    document.getElementById('address_line_1').innerText = await response.location["region"];
+    document.getElementById('address_line_2').innerText = await response.location["city"];
+    document.getElementById('timezone_final').innerText = await response.location["timezone"];
+    document.getElementById('isp_final').innerText = await response.isp;
+
+    let la = await response.location["lat"];
+    let lo = await response.location["lng"];
+
+    
+
+    var map = L.map('map').setView([la, lo], 13);
+                        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                            maxZoom: 19,
+                            attribution: '© OpenStreetMap'
+                        }).addTo(map);
+    
+    
+    var marker = L.marker([la, lo]).addTo(map);
+    return response;
+}
 
 
 handleUserIp();
+
+locateIp();
+
+
+
+
+
+
+const searchInput = document.getElementById("ip_input")
+    searchInput.addEventListener("keypress", function onEvent(event) {
+    if (event.key === "Enter") {
+        document.getElementById("go_button").click();
+    }
+    });
+
+
