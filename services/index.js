173f634f5f2f611e6dc9ded2543e29f022cc9081
Eduardo <eduhmq@gmail.com>
2022-08-05 00:06:13 -0300

    Finished geolocation feature

--- services/index.js
+++ services/index.js
@@ -1,3 +1,5 @@
+var map = L.map('map')
+
 async function getIp() {
     let response = await fetch('https://api.ipify.org/?format=json')
     response = await response.json()
@@ -8,7 +10,10 @@ async function handleUserIp() {
     const ip = await this.getIp();
     console.log(ip)
     document.getElementById("ip_final").innerText = ip;
+
+   
     doSearch();
+
     return ip;
 }
 
@@ -22,7 +27,7 @@ async function locateIp() {
 async function doSearch() {
     let ipToSearch = document.getElementById('ip_input').value;
     console.log(ipToSearch);
-    let ipUrl = 'https://geo.ipify.org/api/v2/country,city?apiKey=at_5FgIKynfFiC4oIrWhRgEzEbHfaTvu&ipAddress='.concat(ipToSearch);
+    let ipUrl = 'https://geo.ipify.org/api/v2/country,city?apiKey=at_8yaIan7ed8aF9tChnm3DZ4BxLFe61&ipAddress='.concat(ipToSearch);
     console.log(ipUrl);
     response = await fetch(ipUrl);
     response = await response.json();
@@ -34,17 +39,20 @@ async function doSearch() {
 
     let la = await response.location["lat"];
     let lo = await response.location["lng"];
-
     
 
-    var map = L.map('map').setView([la, lo], 13);
-                        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                            maxZoom: 19,
-                            attribution: '© OpenStreetMap'
-                        }).addTo(map);
-    
-    
-    var marker = L.marker([la, lo]).addTo(map);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '© OpenStreetMap'
+    }).addTo(map);
+    function mapSetView() {
+
+        map.setView([la, lo], 13);
+        var marker = L.marker([la, lo]).addTo(map);
+    }
+    mapSetView();
+
     return response;
 }
 
@@ -59,10 +67,10 @@ locateIp();
 
 
 const searchInput = document.getElementById("ip_input")
-    searchInput.addEventListener("keypress", function onEvent(event) {
+searchInput.addEventListener("keypress", function onEvent(event) {
     if (event.key === "Enter") {
         document.getElementById("go_button").click();
     }
-    });
+});
 
 
